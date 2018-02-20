67cdfc40268c6f7fa297be30ab0b2ad01d8a331a
limv003 <36581793+limv003@users.noreply.github.com>
2018-02-19 22:11:52 -0800

    Add files via upload

---
+++ memory_game/js/main.js
@@ -0,0 +1,11 @@
+console.log("Up and running!");
+
+// naming each of my 4 cards, two queens and two kings
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+//display the value of the two cards the user has flipped over
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
