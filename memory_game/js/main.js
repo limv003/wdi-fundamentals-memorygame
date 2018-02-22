8f88b6d94627324395bc9de40cc5fafaa60094fa
limv003 <36581793+limv003@users.noreply.github.com>
2018-02-21 21:44:14 -0800

    Add files via upload

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,26 @@
 //unit 8 assignment: create an array of cards and alert for a match
-var cards = ["queen","queen", "king", "king"];
+var cards = [
+  {
+    rank: "queen",
+    suit: "hearts",
+    cardImage: "images/queen-of-hearts.png",
+  },
+  {
+    rank: "queen",
+    suit: "diamonds",
+    cardImage: "images/queen-of-diamonds.png",
+  },
+  {
+    rank: "king",
+    suit: "hearts",
+    cardImage: "images/king-of-hearts.png",
+  },
+  {
+    rank: "king",
+    suit: "diamonds",
+    cardImage: "images/king-of-diamonds.png",
+  }];
+
 var cardsInPlay = [];
 
 //unit 9 assignment: create a function for flipping cards
@@ -12,8 +33,10 @@ var checkForMatch = function () {
 }
 
 var flipCard = function (cardId) {
-	console.log("User flipped " + cards[cardId] + ".");
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank + ".");
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 	checkForMatch();
 };
 
