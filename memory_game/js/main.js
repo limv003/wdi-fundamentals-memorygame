ef3b48f384cb61d2b5c2a9ef403770d3447610ed
limv003 <36581793+limv003@users.noreply.github.com>
2018-02-21 21:34:58 -0800

    Add files via upload

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,19 +2,20 @@
 var cards = ["queen","queen", "king", "king"];
 var cardsInPlay = [];
 
-//create variable to show 1st card a user has flipped
-var cardOne = cards[0];
-var cardTwo = cards[2];
+//unit 9 assignment: create a function for flipping cards
+var checkForMatch = function () {
+  	if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+	} else {
+	alert("Sorry, try again.");
+	};
+}
 
-//add 1st card to the cardsInPlay array
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
-
-//check to make sure user has flipped two cards & they match
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay [1]) {
-		alert("You found a match!");
-} else {
-		alert("Sorry, try again.");
+var flipCard = function (cardId) {
+	console.log("User flipped " + cards[cardId] + ".");
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
 };
+
+flipCard(0);
+flipCard(2);
